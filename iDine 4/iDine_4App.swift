399a0f558b69fc3eccf07edda4df442c42da5a64
Caden Christesen <baptistchristesen@gmail.com>
2023-09-26 09:36:52 -0500

    Initial Commit

---
+++ iDine 4/iDine_4App.swift
@@ -0,0 +1,17 @@
+//
+//  iDine_4App.swift
+//  iDine 4
+//
+//  Created by Caden Christesen on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct iDine_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iDine 4UITests/iDine_4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iDine_4UITestsLaunchTests.swift
+//  iDine 4UITests
+//
+//  Created by Caden Christesen on 9/26/23.
+//
+
+import XCTest
+
+final class iDine_4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
