61316abbd688a044040f0de5a09cfedab448998a
Caden Christesen <baptistchristesen@gmail.com>
2023-10-11 09:31:00 -0500

    like did the first steps and item row stuff

---
+++ iDine 4/ItemRow.swift
@@ -0,0 +1,18 @@
+//
+//  ItemRow.swift
+//  iDine 4
+//
+//  Created by Caden Christesen on 10/11/23.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemRow()
+}
---
+++ iDine 4/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  iDine 4
+//
+//  Created by Caden Christesen on 10/11/23.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
